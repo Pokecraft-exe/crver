3ea5ca070514413c1dbe0351d933b1ea5ef575fe
Julian Lavis--Fabbri <leviathancorp.officiel@gmail.com>
2025-04-24 22:42:35 +0200

    crver api 2.0

--- crver.hpp
+++ crver.hpp
@@ -16,30 +16,7 @@
 #include <stdexcept>
 #include <cstdio>
 
-#define MacroHTTPResponse(DATE, CONTENT, LEN) "HTTP/1.1 200 OK\n" 							\
-						<< "Date : " << std::put_time(DATE, (char*)"%a, %d %b %Y %T %Z")    \
-						<< "\nServer: c_rver / 1.0.0 (Windows)\n"                           \
-						<< "Content-Type: text/html\n"                                      \
-						<< "Content-Length:" << std::to_string(LEN)                         \
-						<< "Connection: keep-alive\n"                                       \
-						<< "Referrer-Policy: strict-origin-when-cross-origin\n"             \
-						<< "X-Content-Type-Options: nosniff\n"                              \
-						<< "Feature-Policy: accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'\n" \
-						<< "Content-Security-Policy: default-src 'self'; script-src cdnjs.cloudflare.com 'self'; style-src cdnjs.cloudflare.com 'self' fonts.googleapis.com 'unsafe-inline'; font-src fonts.googleapis.com fonts.gstatic.com cdnjs.cloudflare.com; frame-ancestors 'none'; report-uri https://scotthelme.report-uri.com/r/d/csp/enforce\r\n\r\n" \
-						<< CONTENT << "\r\n"
-
-#define MacroHTTP404(DATE) "HTTP/1.1 404 Not Found\n"					                      \
-						<< "Date : " << std::put_time(DATE, (char*)"%a, %d %b %Y %T %Z")        \
-						<< "\nServer: c_rver / 1.0.0 (Windows)\n"                               \
-						<< "Connection: keep-alive\n"                                           \
-						<< "Referrer-Policy: strict-origin-when-cross-origin\r\n"               \
-						<< "Content-Type: text/html\n"                                          \
-						<< "Content-Length: 149\n"                                              \
-						<< "X-Content-Type-Options: nosniff\r\n\r\n"                            \
-						<< "<html><head><title>404 Not Found</title></head><body><center>"      \
-						<< "<h1>404 Not Found</h1></center><hr><center>c_rver/1.0.0 (Windows)"  \
-						<< "</center></body></html>\r\n"
-
+#define VERSION "c_rver API / 2.0.0 (Windows)"
 #define MakeEnvironnemnt(x) ::crver::HTTPENV x;::crver::makeHTTPENV(&x, argv)
 
 namespace crver {
@@ -132,21 +109,129 @@ namespace crver {
 		return;
 	}
 
-	int HTTPResponse() {
+	std::map<int, const char*> status_code_str = {
+		{100, "Continue"},
+		{101, "Switching Protocols"},
+		{102, "Processing"},
+		{103, "Early Hints"},
+		{200, "OK"},
+		{201, "Created"},
+		{202, "Accepted"},
+		{203, "Non-Authoritative Information"},
+		{204, "No Content"},
+		{205, "Reset Content"},
+		{206, "Partial Content"},
+		{207, "Multi-Status"},
+		{208, "Already Reported"},
+		{226, "IM Used"},
+		{300, "Multiple Choices"},
+		{301, "Moved Permanently"},
+		{302, "Found"},
+		{303, "See Other"},
+		{304, "Not Modified"},
+		{305, "Use Proxy"},
+		{306, "unused"},
+		{307, "Temporary Redirect"},
+		{308, "Permanant Redirect"},
+		{400, "Bad Request"},
+		{401, "Unauthorized"},
+		{402, "Payment Required"},
+		{403, "Forbidden"},
+		{404, "Not Found"},
+		{405, "Method Not Allowed"},
+		{406, "Not Acceptable"},
+		{407, "Proxy Authentification Required"},
+		{408, "Request Timeout"},
+		{409, "Conflicts"},
+		{410, "Gone"},
+		{411, "Length Required"},
+		{412, "Precondition Failed"},
+		{413, "Content Too Large"},
+		{414, "URI Too Long"},
+		{415, "Unsupported Media Type"},
+		{416, "Range Not Satisfiable"},
+		{417, "Expectation Failed"},
+		{418, "I'm a teapot"},
+		{421, "Misdirected Request"},
+		{422, "Unprocessable Content"},
+		{423, "Locked"},
+		{424, "Failed Dependancy"},
+		{425, "Too Early"},
+		{426, "Upgrade Required"},
+		{428, "Precondition Required"},
+		{429, "Too Many Requests"},
+		{431, "Request Header Fields Too Large"},
+		{451, "Unavaliable For Legal Reasons"},
+		{500, "Internal Server Error"},
+		{501, "Not Implemented"},
+		{502, "Bad Gateway"},
+		{503, "Service Unavailable"},
+		{504, "Gateway Timeout"},
+		{505, "HTTP Version Not Supported"},
+		{506, "Variant Also Negotiates"},
+		{508, "Insufficient Storage"},
+		{509, "Loop Detected"},
+		{510, "Not Extended"},
+		{511, "Network Authentication Required"},
+	};
+
+	enum class contentType {
+		none = 0,
+		textHTML,
+		textCSS,
+		textJS
+	};
+
+	std::string header = "Connection: keep-alive\n"
+						"Referrer-Policy: strict-origin-when-cross-origin\n"
+						"X-Content-Type-Options: nosniff";
+
+	std::string head = "";
+
+	void addHeader(std::string newHeader){
+		header += '\n' + newHeader;
+		return;
+	}
+	 void link(std::string rel, std::string type, std::string href) {
+		head += "<link rel=\"" + rel + "\" type=\"" + type + "\" href=\"" + href + "\" />";
+		return;
+	}
+	void title(std::string title) {
+		head += "<title>" + title + "</title>";
+		return;
+	}
+	 void meta(std::string name, std::string content, std::string href) {
+		head += "<meta name=\"" + name + "\" content=\"" + content + "\" />";
+		return;
+	}
+	 void metaCharset(std::string charset) {
+		head += "<meta charset=\"" + charset + "\" />";
+		return;
+	}
+
+	int HTTPResponse(int status_code, std::string content_type) {
 		::std::cout.rdbuf(stdoutBuffer);
 
 		std::time_t t_date = time(nullptr);
-		std::cout << MacroHTTPResponse(gmtime(&t_date), __HTTPbuffer__.str(), __HTTPbuffer__.str().length());
+		std::cout << "HTTP/1.1 " << status_code << " " << status_code_str[status_code] << "\n"
+						"Date : " << std::put_time(gmtime(&t_date), (char*)"%a, %d %b %Y %T %Z") <<
+						"\nServer: " VERSION "\n"
+						"Content-Type: " << content_type << "\n"
+						"Content-Length:" << std::to_string(__HTTPbuffer__.str().length() + 43 + head.length()) << '\n'
+						<< header << "\r\n\r\n\r\n" << "<!DOCTYPE html><html><head>" << head << "</head>" <<
+						__HTTPbuffer__.str() << "</html>\r\n";
 
 		return 0;
 	};
 
 	int HTTP404() {
-		::std::cout.rdbuf(stdoutBuffer);
+		std::cout.clear();
 
-		std::time_t t_date = time(nullptr);
-		std::cout << MacroHTTP404(gmtime(&t_date));
+		std::cout << 
+		"<html><head><title>404 Not Found</title></head>"
+		"<body><center><h1>404 Not Found</h1><br>c_rver/1.0.0 (Windows)"
+		"</center></body></html>";
 
-		return 1;
+		return HTTPResponse(404, "text/html");
 	}
 }
