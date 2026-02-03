8f1ca9e4950fe06022145d93e84684bfdee7bddd
Pokecraft-exe <leviathancorp.officiel@gmail.com>
2026-02-03 01:19:17 +0100

    fix/ linux full support

---
+++ include/Worker.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "Session.hpp"
+#include <thread>
+#include <vector>
+
+class Worker {
+private:
+    Session* CurrentSession = nullptr;
+    bool  waiting = false;
+	Worker* next = nullptr;
+    std::thread thread = {};
+    void* s = nullptr;
+public:
+    bool  active = false;
+    bool  should_stop = false;
+
+    bool initialize(void* s);
+    bool createNewWorker();
+	bool isWaiting() const { return waiting; }
+	Worker* getNextWorker();
+    static bool work(Worker* _this);
+};
+
+extern Worker* Workers;
+extern int WorkersCount;
